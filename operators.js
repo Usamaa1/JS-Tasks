893ccde501d4e859b1b1be931c314b506b837b23
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2020-01-23 23:59:04 +0500

    JavaScript Operators Task
    In this file all JS operators and their functions was performed.

---
+++ operators.js
@@ -0,0 +1,42 @@
+/**********Operators */
+// var year, fawadAge, usamaAge;
+
+year = 2020;
+fawadAge = 28;
+usamaAge = 21;
+
+city = "karachi"
+
+yearFawad = year - fawadAge;
+
+yearUsama = year -  usamaAge;
+
+console.log(yearFawad);
+console.log(yearUsama);
+
+console.log(fawadAge -7);
+console.log(usamaAge * 2)
+
+//logical operator
+
+console.log(fawadAge > usamaAge);
+console.log(usamaAge > fawadAge);
+
+b = 2+2-6+10/8+100;
+console.log(b)
+
+//typeof function for operators
+console.log(typeof fawadAge > usamaAge);
+
+console.log(typeof 2 * false)
+console.log(typeof city)
+
+
+
+console.log(typeof 10/2*6+4);
+console.log(typeof 3)
+console.log(typeof b)
+console.log(typeof x);
+
+var a 
+console.log(typeof a)
