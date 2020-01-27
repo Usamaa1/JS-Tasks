d60371a15a222fc7f59abe05d8a00a7cc0bbe422
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2020-01-27 22:57:40 +0500

    Operators Precedence
    In this file all operators precedence was defined and performed through javascript

---
+++ operatorsPrecedence.js
@@ -0,0 +1,37 @@
+ 
+ 
+ ////Operators Precedence
+ 
+ now = 2020;
+ yearUsama = 1997;
+ age = 2
+ fullAge = now - yearUsama >= age;
+console.log(fullAge);
+
+//Grouping
+ageUsama = now - yearUsama;
+ageFaiq = 29;
+avg = (ageFaiq - ageUsama) /2;
+console.log(avg);
+
+//multiple assignments
+
+var x, y;
+x = y = (5 * 8) + 7 - 2;
+console.log(x, y);
+
+//more operators
+
+x *= 5;
+console.log(x);
+x += 10;
+console.log(x);
+x -= 6;
+console.log(x);
+
+y++;
+console.log(y);
+y--;
+console.log(y);
+y /= 8;
+console.log(y);
